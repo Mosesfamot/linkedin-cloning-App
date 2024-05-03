fe167d35c8e9133c7a16545c3831b9b86d840cb3
Moses Ojo <mosesadegoke.jo@gmail.com>
2024-05-03 19:25:25 +0100

    add media queries

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 let profileMenu = document.getElementById("profileMenu");
+let sideActivity = document.getElementById("sidebarActivity");
+let moreLink = document.getElementById("showMoreLink");
 
 function toggleMenu() {
     profileMenu.classList.toggle("open-menu");
 }
+
+function toggleActivity() {
+    sidebarActivity.classList.toggle("open-activity");
+}
+
