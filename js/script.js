21f90164bd1b0e764eeccfc71cb16bd60d5a3fc3
Moses Ojo <mosesadegoke.jo@gmail.com>
2024-07-19 18:33:12 +0100

    adding other pages

--- js/script.js
+++ js/script.js
@@ -132,8 +132,9 @@ function getHardResponse(userText) {
     let botHtml = 
     '<img src="img/user-3.png" alt="User Image">' +
     '<article class="user-chat-time">' +
-    '<h4>James Brown . <span id="get-time"></span></h4>' + 
-    botResponse + "</span></p>";
+    '<h4>James Brown . <span id="get-time"></span></h4>' +
+    '<p id="starter-message">' + 
+    botResponse + "</p>";
     $("#chat-box").append(botHtml);
   
     document.getElementById("chat-bar-bottom").scrollIntoView(true);
