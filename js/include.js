30500b3c5914ddead02be070ee06f6cd0c9f8db1
Moses Ojo <mosesadegoke.jo@gmail.com>
2024-08-17 15:29:00 +0100

    adding message popup

--- js/include.js
+++ js/include.js
@@ -208,5 +208,239 @@ class pageNavbar extends HTMLElement {
     }
 };
 
-
 customElements.define('include-navbar', pageNavbar);
+
+
+class messagetabPop extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <section class="message-tab-pop">
+            
+            <article class="message-tab-pop-icon">
+                <article class="popup-profile">
+                    <img src="img/user-1.png" alt="User-image">
+                    <span>Messaging</span>
+                    <i class='bx bx-dots-horizontal-rounded'></i>
+                    <i class='bx bxs-edit'></i>
+                    <i class='bx bx-chevron-down'></i>
+                </article>
+            </article>
+
+            <article class="message-tab-pop-bg">
+
+                <article class="message-tab-pop-container">
+
+                    <article class="search-box-container">
+                        <article class="search-box">
+                            <i class='bx bx-search' ></i>
+                            <input type="text" placeholder="Search messages">
+                            <i class='bx bx-slider'></i>
+                        </article>
+                    </article>
+        
+                    <article class="message-popup-tabs">
+                        <ul class="message-tab-pop-list">
+                            <li class="message-tab-focused">Focused</li>
+                            <li class="message-tab-other">Other</li>
+                        </ul>
+                    </article>
+    
+                    <article class="message-popup-focused-content tab-focused-content active">
+                        <img src="img/user-2.png" alt="User-Image">
+                        <article class="message-content-highlight">
+                            <h4>Blessing Gabriel <span>Jun 31, 2024</span></h4>
+                            <p>Blessing: Okay</p>
+                        </article>
+                    </article>
+                    
+                    <article class="message-popup-focused-content tab-focused-content active">
+                        <img src="img/user-3.png" alt="User-Image">
+                        <article class="message-content-highlight">
+                            <h4>Johnson Brown <span>Jul 15, 2024</span></h4>
+                            <p>Johnson: You qualify for...</p>
+                        </article>
+                    </article>
+
+                    <article class="message-popup-other-content tab-other-content">
+                        <img src="img/message-image.svg" alt="Message-Image">
+                        <h3>No messages yet</h3>
+                        <p>Reach out and start a conversation to advance your career</p>
+                        <a href="#">Send a message</a>
+                    </article>
+
+                </article>    
+
+            </article>
+        </section>
+        `
+    }
+};
+
+customElements.define('messagetab-pop', messagetabPop);
+
+
+class messagetabChatPop extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <section class="message-tab-chatpop">
+
+            <article class="message-tab-chatpop-icon">
+                <article class="message-tab-chatpop-profile">
+                    <article class="chatpopup-profile">
+                        <img src="img/user-2.png" alt="User-image">
+                        <span>Blessing Gabriel</span>
+                    </article>
+                    <article class="chatpop-icon">
+                        <i class='bx bx-dots-horizontal-rounded'></i>
+                        <i class='bx bxs-video-plus'></i>
+                        <i class='bx bx-collapse'></i>
+                        <i class='bx bx-x'></i>
+                    </article>
+                    <!-- <hr> -->
+                </article>
+            </article>
+
+            <article class="message-tab-chatpop-bg">
+                
+                <article class="chatpop-box-container">
+
+                    <article class="message-tab-full-chat">
+    
+                        <article id="scroll-to-bottom" class="full-message-chat" style="height: 50vh;">
+                            <article class="full-message-chat-profile">
+                                <img src="img/user-2.png" alt="Image">
+                                <h4>Blessing Gabriel . <span>1st</span></h4>
+                                <p>Team Lead at Quest Co</p>
+                            </article>
+                            
+                            <article id="chat-box" class="full-chats-section">
+                                <article class="full-chat-date">
+                                    <hr>
+                                    <p>JUN 20, 2024</p>
+                                    <hr>
+                                </article>
+                                
+                                <article class="chats-by-users">
+                                    <img src="img/user-2.png" alt="User Image">
+                                    <article class="user-chat-time">
+                                        <h4>Blessing Gabriel . <span>02:20 PM</span></h4>
+                                        <p>Thanks for connecting! How are you?</p>
+                                    </article>
+                                </article>
+
+                                <article class="chats-by-users">
+                                    <img src="img/user-1.png" alt="User Image">
+                                    <article class="user-chat-time">
+                                        <h4>Moses Ojo . <span>02:25 PM</span></h4>
+                                        <p>It's my pleasure. I'm fine thank you. And you?</p>
+                                    </article>
+                                </article>
+
+                                <article class="chats-by-users">
+                                    <img src="img/user-2.png" alt="User Image">
+                                    <article class="user-chat-time">
+                                        <h4>Blessing Gabriel . <span>02:28 PM</span></h4>
+                                        <p>
+                                            I'm good. 
+                                            <br>
+                                            I think AR has so much potential, especially with how immersive it's getting.
+                                            <br>
+                                            Did you hear anything about the rumored Apple car?
+                                        </p>
+                                    </article>
+                                </article>
+
+                                <article class="chats-by-users">
+                                    <img src="img/user-1.png" alt="User Image">
+                                    <article class="user-chat-time">
+                                        <h4>Moses Ojo . <span>02:30 PM</span></h4>
+                                        <p>
+                                            A little bit, but it's still pretty hush-hush. 
+                                            <br>
+                                            I'm guessing it's going to be autonomou with some crazy AI integration.                                            
+                                        </p>
+                                    </article>
+                                </article>
+
+                                <article class="chats-by-users">
+                                    <img src="img/user-2.png" alt="User Image">
+                                    <article class="user-chat-time">
+                                        <h4>Blessing Gabriel . <span>02:32 PM</span></h4>
+                                        <p>
+                                            Couldn't agree more. Here's to more innovations and awesome tech!
+                                        </p>
+                                    </article>
+                                </article>
+
+                                <article class="chats-by-users">
+                                    <img src="img/user-1.png" alt="User Image">
+                                    <article class="user-chat-time">
+                                        <h4>Moses Ojo . <span>02:33 PM</span></h4>
+                                        <p>&#x1F44D;</p>
+                                    </article>
+                                </article>
+
+                                <article class="full-chat-date">
+                                    <hr>
+                                    <p class="chat-full-date"></p>
+                                    <hr>
+                                </article>
+
+                                <article class="chats-by-users">
+                                    <img src="img/user-2.png" alt="User Image">
+                                    <article class="user-chat-time">
+                                        <h4>Blessing Gabriel . <span id="get-time"></span></h4>
+                                        <p id="starter-message"></p>
+                                    </article>
+                                </article>
+
+                                <article class="chatbox incoming">
+                                    <!-- <article class="chats-by-users">
+                                        <img src="img/user-3.png" alt="User Image">
+                                        <article class="user-chat-time">
+                                            <h4>James Brown . <span id="get-time"></span></h4>
+                                            <p id="starter-message"></p>
+                                        </article>
+                                    </article> -->
+                                </article>
+
+                            </article>
+                        </article>
+                    </article>
+
+                    <article class="message-chat-input">
+
+                        <article class="chat-input-area">
+                            <input id="textInput" class="input-box" type="text" placeholder="Write a message...">
+                            <i class='bx bx-chevron-up'></i>
+                        </article>
+                        <hr>
+                        
+
+                        <article class="message-chat-footer">
+                            
+                            <article class="chat-footer-icon">
+                                <i class='bx bx-image bx-flip-horizontal'></i>
+                                <i class='bx bx-paperclip'></i>
+                                <i class='bx bxs-file-gif'></i>
+                                <i class='bx bx-smile' onclick="smileButton()"></i>
+
+                                <!-- <article class="chat-footer-icon chat-footer-action"> -->
+                                    <a href="#">Send</a>
+                                    <i class='bx bx-dots-horizontal-rounded message-option-icon'></i>
+                                <!-- </article> -->
+                            </article>
+                            
+                        </article>
+                    </article>
+
+                </article>
+
+            </article>
+
+        </section>
+        `
+    }
+};
+
+customElements.define('messagetab-chatpop', messagetabChatPop);
